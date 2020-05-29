3aeb6d72720dc5737470d77f0c50a36edb1d100d
Marcin Chmiel <marcin.k.chmiel@gmail.com>
2020-05-29 19:24:47 +0200

    Add Carousel component

---
+++ components/Carousel.vue
@@ -0,0 +1,25 @@
+<template>
+<b-carousel
+  :id="id"
+  v-model="slide"
+  :interval="interval || 5000"
+  :no-animation="noanim"
+  :fade="fade"
+  indicators
+  >
+  <b-carousel-slide
+    v-for="img in images"
+    :img-src="img"
+    :key="img"
+    ></b-carousel-slide>
+</b-carousel>
+</template>
+
+<script>
+export default {
+  props: ['images', 'id', 'interval', 'noanim', 'fade'],
+  data() {
+    return { slide: null }
+  }
+}
+</script>
