2426e65230c1376b256e61265fa0da8b423a271a
Marcin Chmiel <marcin.k.chmiel@gmail.com>
2020-06-22 03:24:01 +0200

    Add Features component

---
+++ components/Features.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h3>Product description</h3>
+  <p>Meet the Conundrum &ndash; world's first 40%, ortholinear, programmable, electro-capacitive keyboard.</p>
+  <p>Conundrum comes as a partially-assembled keyboard collector's kit.</p>
+  <p>
+    Each kit comes with:
+    <ul>
+      <li>aluminum top piece</li>
+      <li>brass plate</li>
+      <li>aluminum bottom piece</li>
+      <li>brass weight</li>
+      <li>electro-capacitive PCB</li>
+      <li>electro-capacitive rubber domes</li>
+      <li>electro-capacitive switch housings</li>
+      <li>electro-capacitive switch sliders</li>
+      <li>electro-capacitive switch springs</li>
+      <li>rubber feet</li>
+      <li>screws</li>
+    </ul>
+  </p>
+  <p>Assembled kit weighs appoximately 1400g.</p>
+  <p>Plate supports both grid and MIT layouts and includes a cutout for a 2u Costar-style stabilizer.</p>
+  <p>Pre-assembled PCB hosts an ARM M4F chipset and a USB-C connector.</p>
+</div>
+</template>
