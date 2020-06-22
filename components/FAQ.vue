95df1125054bc2bc3953094297f8ef1644fe59a1
Marcin Chmiel <marcin.k.chmiel@gmail.com>
2020-06-22 03:24:14 +0200

    Add FAQ component

---
+++ components/FAQ.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  <h3>Frequently Asked Questions</h3>
+  <p><em>Q: What is the expected date of shipment?</em></p>
+  <p>A: Packages will be shipped out in the first half of Q4 2020 &ndash; or sooner, depending on the schedule.</p>
+  <p><em>Q: Does the kit contain a keycap set?</em></p>
+  <p>A: No, keycaps are not included.</p>
+  <p><em>Q: What does the price I specify mean?</em></p>
+  <p>A: In order to ensure a fair price and make sure as many people as possible can enjoy the Conundrum keyboard this Group Buy is run in a form of Vickrey auction. This means that the price you specify is the maximum amount that you're willing to pay. Based on all the entries a price will be selected. If your price is above the selected price your card will be charged the lower price and order delivered within the timeline above. If your price is below the selected price you won't be charged at all and your entry will be excluded from the Group Buy.</p>
+</div>
+</template>
