684290a76cd74a29763c0b18e10eedfba462c443
Marcin Chmiel <marcin.k.chmiel@gmail.com>
2020-06-22 03:22:41 +0200

    Separate custom SCSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
   ** Global CSS
   */
   css: [
+      '~/assets/custom.scss'
   ],
   /*
   ** Plugins to load before mounting the App
--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@
       <b-container fluid class="my-3">
         <b-row>
           <b-col>
-            <h3>Get notified when the group buy goes live</h3>
+            <h3 class="text-center">Get notified when the group buy goes live</h3>
           </b-col>
         </b-row>
       </b-container>
@@ -152,7 +152,7 @@
       <b-container fluid class="my-3">
         <b-row>
           <b-col>
-            <h3>Get notified when the group buy goes live</h3>
+            <h3 class="text-center">Get notified when the group buy goes live</h3>
           </b-col>
         </b-row>
       </b-container>
@@ -231,17 +231,6 @@ export default {
 </script>
 
 <style lang="scss">
-  @import '~bootstrap/scss/bootstrap.scss';
-  @import '~bootstrap-vue/src/index.scss';
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 1.5s ease-in-out;
-}
-
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-}
-
 .fade2-enter-active, .fade2-leave-active {
   transition: opacity 3s ease-in-out;
   transition-delay: 0.5s;
@@ -251,105 +240,13 @@ export default {
   opacity: 0;
 }
 
-.rel {
-    position: relative;
-    width: 100%;
-    height: 0px;
-}
-
-svg.bottom {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 6vw;
-  /* set height to pixels if you want angle to change with screen width */
-}
-
-svg.top {
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 6vw;
-  /* set height to pixels if you want angle to change with screen width */
-}
-
 .section-title {
     position: relative;
-}
-
-header, {
-    position: relative;
-    padding-bottom: 3vw;
-    background: #ffb347;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #ffcc33, #ffb347);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #ffcc33, #ffb347); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-}
-
-.container {
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.main {
-  margin: 0 auto;
-  min-height: 40vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 6rem;
-  color: rgba(255, 255, 255, 0.85);
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  margin-top: -24px;
-  opacity: 0.7;
-  font-weight: 100;
-  font-size: 2.4rem;
-  word-spacing: 5px;
-  /* padding-bottom: 15px; */
 }
 
 .details {
     .row {
         align-items: center;
     }
-}
-
-html {
-  font-size: 0.65em;
-}
-
-@include media-breakpoint-up(sm) {
-  html {
-    font-size: 0.7rem;
-  }
-}
-
-@include media-breakpoint-up(md) {
-  html {
-    font-size: 0.8rem;
-  }
-}
-
-@include media-breakpoint-up(lg) {
-  html {
-    font-size: 1rem;
-  }
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
