9b4288833d4087d459a6fda3b7ce8be28b8032e6
Marcin Chmiel <marcin.k.chmiel@gmail.com>
2020-09-20 23:47:46 +0200

    Update FAQ

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -2,7 +2,9 @@
 <div>
   <h3>Frequently Asked Questions</h3>
   <p><em>Q: What is the expected date of shipment?</em></p>
-  <p>A: Packages will be shipped out in the first half of Q4 2020 &ndash; or sooner, depending on the schedule.</p>
+  <p>A: Packages will be shipped out in the first half of Q1 2021 &ndash; or sooner, depending on the schedule.</p>
+  <p><em>Q: Does it support MX-style keycaps?</em></p>
+  <p>A: Yes, the sliders support MX-style keycaps. Topre-style keycaps are not supported.</p>
   <p><em>Q: Does the kit contain a keycap set?</em></p>
   <p>A: No, keycaps are not included.</p>
 </div>
