5c2ee43321523cfb6bd0b88de04ee38aa479b6fa
Marcin Chmiel <marcin.k.chmiel@gmail.com>
2020-12-18 07:25:35 +0100

    Update FAQ and features

--- components/FAQ.vue
+++ components/FAQ.vue
@@ -1,11 +1,28 @@
 <template>
 <div>
   <h3>Frequently Asked Questions</h3>
-  <p><em>Q: What is the expected date of shipment?</em></p>
-  <p>A: Packages will be shipped out in the first half of Q1 2021 &ndash; or sooner, depending on the schedule.</p>
-  <p><em>Q: Does it support MX-style keycaps?</em></p>
-  <p>A: Yes, the sliders support MX-style keycaps. Topre-style keycaps are not supported.</p>
-  <p><em>Q: Does the kit contain a keycap set?</em></p>
-  <p>A: No, keycaps are not included.</p>
+
+  <p><em><strong>Q:</strong> Do I have to obtain the switches separately?</em></p>
+  <p><strong>A:</strong> No, a set of electro-capacitive switches is included as part of the kit. Circumventing the need to buy $300+ Topre keyboards just to salvage them for parts has been a core goal from day one.</p>
+  <hr/>
+
+  <p><em><strong>Q:</strong> What layouts are supported?</em></p>
+  <p><strong>A:</strong> Both grid and MIT layouts are supported. The plate has a 2u cutout that can accomodate either a single 2u switch or two 1u switches.</p>
+  <hr/>
+
+  <p><em><strong>Q:</strong> What is the expected date of shipment?</em></p>
+  <p><strong>A:</strong> Q3 2021 &ndash; or sooner if the schedule allows.</p>
+  <hr/>
+
+  <p><em><strong>Q:</strong> Does it support MX-style keycaps?</em></p>
+  <p><strong>A:</strong> Yes, the sliders support MX-style keycaps &ndash; you can use all your favorite keycaps. Topre-style keycaps are not supported.</p>
+  <hr/>
+
+  <p><em><strong>Q:</strong> Does the kit contain a keycap set?</em></p>
+  <p><strong>A:</strong> No, keycaps are not included.</p>
+  <hr/>
+
+  <p><em><strong>Q:</strong> Why is it so expensive?</em></p>
+  <p><strong>A:</strong> This is the first keyboard to provide a fully custom, programmable, electro-capacitive experience. Bringing this keyboard to life required years of research, prototyping and firmware development. Setting up a whole new branch of keyboards requires significant investments. In order to enable future EC projects, profits from this Group Buy are going to fund these investments.</p>
 </div>
 </template>
--- components/Features.vue
+++ components/Features.vue
@@ -6,25 +6,24 @@
   <p>
     Each kit comes in a box with:
     <ul>
-      <li>aluminum top piece</li>
-      <li>brass plate</li>
-      <li>aluminum bottom piece</li>
-      <li>brass weight</li>
-      <li>electro-capacitive PCB</li>
+      <li>an e-coated aluminum top piece</li>
+      <li>a PVD-coated polished brass plate</li>
+      <li>an e-coated aluminum bottom piece</li>
+      <li>a PVD-coated polished brass weight</li>
+      <li>a soldered electro-capacitive PCB that hosts a powerful ARM M4F chipset and a USB-C connector</li>
       <li>48x electro-capacitive rubber domes</li>
       <li>48x electro-capacitive switch housings</li>
-      <li>48x electro-capacitive switch sliders (with O-rings)</li>
+      <li>48x electro-capacitive switch sliders (with O-rings, MX-compatible)</li>
       <li>48x electro-capacitive switch springs</li>
       <li>4x rubber feet</li>
       <li>screws</li>
-      <li>carrying case</li>
-      <li>USB-C cable</li>
-      <li>brass polishing cloth</li>
-      <li>easy assembly station</li>
+      <li>a carrying case</li>
+      <li>a USB-C cable</li>
+      <li>a brass polishing cloth</li>
+      <li>an easy assembly station</li>
     </ul>
   </p>
   <p>Assembled kit weighs appoximately 1400g.</p>
-  <p>Plate supports both grid and MIT layouts and includes a cutout for a 2u Costar-style stabilizer.</p>
-  <p>Pre-assembled PCB hosts an ARM M4F chipset and a USB-C connector.</p>
+  <p>Plate has a 2u cutout that supports both grid and MIT layouts.</p>
 </div>
 </template>
